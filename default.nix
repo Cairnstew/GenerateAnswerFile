0f00beb6525520e099558947a4af571d78d9be89
Sean Cairns <sean.cairnsst@gmail.com>
2026-02-11 00:19:25 +0000

    Added nix Integration, Via flake.nix and default.nix

---
+++ default.nix
@@ -0,0 +1,40 @@
+{
+  buildDotnetModule,
+  dotnetCorePackages,
+}:
+
+let
+  sdk_8_0 = dotnetCorePackages.sdk_8_0;
+  sdk_9_0 = dotnetCorePackages.sdk_9_0;
+  combinePackages = dotnetCorePackages.combinePackages [
+    sdk_8_0
+    sdk_9_0
+  ];
+in
+buildDotnetModule {
+  pname = "GenerateAnswerFile";
+  version = "2.2";
+
+  # Path to your source directory
+  src = ./src;
+
+  # Relative path to your .csproj file inside src
+  projectFile = [ 
+    "GenerateAnswerFile/GenerateAnswerFile.csproj"
+    "Ookii.AnswerFile/Ookii.AnswerFile.csproj" 
+  ];
+
+  testProjectFiles = [ 
+    "Ookii.AnswerFile.Tests/Ookii.AnswerFile.Tests.csproj" 
+  ];
+  
+  # .NET SDK and runtime versions
+  dotnet-sdk = combinePackages;
+  dotnet-runtime = dotnetCorePackages.runtime_9_0;
+
+  dotnetBuildFlags = [ "--framework net9.0" ];
+  dotnetInstallFlags = [ "--framework net9.0" ];
+
+  # Path to your NuGet deps file (can be generated automatically)
+  nugetDeps = ./deps.json;
+}
---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Flake to build GenerateAnswerFile using existing default.nix";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  
+
+  outputs = { self, nixpkgs }: 
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      
+      dotnetCorePackages = pkgs.dotnetCorePackages;
+    in {
+      packages.x86_64-linux.GenerateAnswerFile = pkgs.callPackage ./default.nix {
+        inherit (pkgs) buildDotnetModule;
+        dotnetCorePackages = dotnetCorePackages;
+      };
+      
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [ (
+          with dotnetCorePackages;
+          combinePackages [
+            sdk_8_0
+            sdk_9_0
+          ]
+        )];
+
+        shellHook = ''
+          # Define a custom command
+          fetch-deps() {
+              echo "Building package.fetch-deps..."
+              local drv
+              drv=$(nix-build -E '
+                let
+                  pkgs = import <nixpkgs> {};
+                in
+                  (import ./default.nix {
+                    inherit (pkgs) buildDotnetModule dotnetCorePackages;
+                  }).fetch-deps
+              ')
+              echo "Running $drv ..."
+              "$drv"
+          }
+           
+          # Optional: show a message when entering the shell
+          echo "Welcome to the development shell! Type 'fetch-deps' to build and run the fetch-deps script."
+        '';
+
+      };
+    };
+}
